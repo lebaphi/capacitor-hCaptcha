0da59c702317c58b4adc4a2ea714fbf9e9d46661
philb89 <everlasting2605@gmail.com>
2021-07-23 14:36:34 +0700

    ios - WIP

--- ios/Plugin/HCaptcha.swift
+++ ios/Plugin/HCaptcha.swift
@@ -7,13 +7,33 @@
 //
 
 import Capacitor
+import HCaptcha
 
 @objc(HCaptchaPlugin)
 public class HCaptchaPlugin: CAPPlugin {
-  @objc func verifyCaptcha(_ call: CAPPluginCall) {
+  
+    
+    @objc func verifyCaptcha(_ call: CAPPluginCall) {
     let siteKey = call.getString("siteKey") ?? ""
-    call.resolve([
-        "siteKey": siteKey
-    ])
+    
+    DispatchQueue.main.sync {
+        let hcaptcha = try? HCaptcha(
+            apiKey: siteKey,
+            baseURL: URL(string: "https://hcaptcha.com")!
+        )
+        
+        let view = self.bridge?.webView
+        
+        hcaptcha?.configureWebView { [weak self] webview in
+            webview.frame = view?.bounds ?? CGRect.zero
+            }
+        
+        hcaptcha?.validate(on: view as! UIView) { [weak self] (result: HCaptchaResult) in
+            call.resolve([
+                "data": result,
+                "status": "success"
+            ])
+        }
+    }
   }
 }
